58f31920f63e94516acda4e335e7f1cf32553137
jmanteau <jmanteau@gmail.com>
2012-12-31 06:46:19 -0800

    Create packages.pp

---
+++ puppet-manifests/modules/kibana/manifests/packages.pp
@@ -0,0 +1,41 @@
+require ruby
+
+package { 'sinatra':
+    ensure   => 'installed',
+    provider => 'gem',
+}
+
+package { 'json':
+    ensure   => 'installed',
+    provider => 'gem',
+}
+
+package { 'fastercsv':
+    ensure   => 'installed',
+    provider => 'gem',
+}
+
+package { 'tzinfo':
+    ensure   => 'installed',
+    provider => 'gem',
+}
+
+package { 'thin':
+    ensure   => 'installed',
+    provider => 'gem',
+}
+
+package { 'rake':
+    ensure   => 'installed',
+    provider => 'gem',
+}
+
+package { 'rspec':
+    ensure   => 'installed',
+    provider => 'gem',
+}
+
+package { 'rspec-mocks':
+    ensure   => 'installed',
+    provider => 'gem',
+}
